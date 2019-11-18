b0f059072ce530341e2efdb927d25e6952e6658f
pyf <910017207@qq.com>
2019-11-18 10:32:26 +0800

    "调整弱引用"

--- utils/src/main/java/com/example/utils/WeakReferenceUtils.java
+++ utils/src/main/java/com/example/utils/WeakReferenceUtils.java
@@ -8,28 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.lang.ref.WeakReference;
 
 public class WeakReferenceUtils {
-    private  WeakReference<Context> reference=null;
-    private  WeakReference<Activity> reference_a=null;
+    private WeakReference<Context> reference = null;
+    private WeakReference<Activity> reference_a = null;
 
-    private static class ObjectUtilsHolder{
-        private static final WeakReferenceUtils INSTANCE=new WeakReferenceUtils();
+    private static class ObjectUtilsHolder {
+        private static final WeakReferenceUtils INSTANCE = new WeakReferenceUtils();
     }
-    private WeakReferenceUtils(){
+
+    private WeakReferenceUtils() {
 
     }
-    public static final WeakReferenceUtils getInstance(){
+
+    public static final WeakReferenceUtils getInstance() {
         return ObjectUtilsHolder.INSTANCE;
     }
-    public void setContext(Context context){
+
+    public Context getContext(Context context) {
         reference = new WeakReference<>(context);
+        return reference.get();
     }
-    public Context getContext(Context context){
-       return reference.get();
-    }
-    public void setContext(Activity mActivity){
+
+    public Activity getContext(Activity mActivity) {
         reference_a = new WeakReference<>(mActivity);
-    }
-    public Activity getContext(Activity mActivity){
         return reference_a.get();
     }
 }
