26ba1feab04bf0160a2fa902b95b4c3f6512bf23
pyf <910017207@qq.com>
2020-01-04 10:38:53 +0800

    "添加延迟事件"

--- utils/src/main/java/com/example/utils/RxTimerUtils.java
+++ utils/src/main/java/com/example/utils/RxTimerUtils.java
@@ -12,12 +12,13 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 // TODO: 2019/11/13 该类待优化
 public class RxTimerUtils {
     private List<Disposable> mDisposableList;
-    private boolean isDebug=false;
-    private String TAG="RxTimerUtils";
+    private boolean isDebug = false;
+    private String TAG = "RxTimerUtils";
 
     private static class RxTimerUtilsHolder {
         private static final RxTimerUtils INSTANCE = new RxTimerUtils();
@@ -33,8 +34,9 @@ public class RxTimerUtils {
 
 
     public void setDebug(Boolean isDebug) {
-        this.isDebug=isDebug;
+        this.isDebug = isDebug;
     }
+
     /**
      * 每隔milliseconds毫秒后执行next操作
      *
@@ -72,12 +74,13 @@ public class RxTimerUtils {
 
     /**
      * 每隔milliseconds毫秒后执行next操作
-     * @param delay 第一次延迟时间
+     *
+     * @param delay        第一次延迟时间
      * @param milliseconds 周期
-     * @param next 结果回调
+     * @param next         结果回调
      */
-    public void interval(long delay,long milliseconds, final IRxNext next) {
-        Observable.interval(delay,milliseconds, TimeUnit.MILLISECONDS)
+    public void interval(long delay, long milliseconds, final IRxNext next) {
+        Observable.interval(delay, milliseconds, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<Long>() {
                     @Override
@@ -104,7 +107,9 @@ public class RxTimerUtils {
                     }
                 });
     }
-    HashMap<String,Disposable> hashMap=new HashMap<>();
+
+    HashMap<String, Disposable> hashMap = new HashMap<>();
+
     public void interval(long milliseconds, final IRxNext next, final String mTag) {
         Observable.interval(milliseconds, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
@@ -112,7 +117,7 @@ public class RxTimerUtils {
                     @Override
                     public void onSubscribe(@NonNull Disposable disposable) {
                         Log.d(TAG, "onSubscribe: ");
-                        hashMap.put(mTag,disposable);
+                        hashMap.put(mTag, disposable);
                     }
 
                     @Override
@@ -133,6 +138,7 @@ public class RxTimerUtils {
                     }
                 });
     }
+
     /**
      * 取消订阅
      */
@@ -144,30 +150,67 @@ public class RxTimerUtils {
             }
         }
         mDisposableList.clear();
-        if (hashMap.keySet()!=null) {
+        if (hashMap.keySet() != null) {
             for (String key : hashMap.keySet()) {
-                Disposable  disposable=  hashMap.get(key);
-                if(!disposable.isDisposed()){
+                Disposable disposable = hashMap.get(key);
+                if (!disposable.isDisposed()) {
                     disposable.dispose();
                 }
                 hashMap.remove(key);
             }
         }
     }
+
     /**
      * 取消指定订阅
      */
     public void cancelTarget(String mTag) {
-        if (hashMap.get(mTag)!=null) {
-            Disposable  disposable=  hashMap.get(mTag);
-            if(!disposable.isDisposed()){
+        if (hashMap.get(mTag) != null) {
+            Disposable disposable = hashMap.get(mTag);
+            if (!disposable.isDisposed()) {
                 disposable.dispose();
             }
-            Log.d(TAG, "cancel"+mTag);
+            Log.d(TAG, "cancel" + mTag);
             hashMap.remove(mTag);
         }
     }
 
+    /**
+     * 延迟执行一次
+     * @param des 传递的描述信息
+     * @param s 秒
+     */
+    public void delay(String des, int s, final IRxNext iRxNext, final String mTag) {
+        Observable.just(des)
+                //延时两秒，第一个参数是数值，第二个参数是事件单位
+                .delay(s, TimeUnit.SECONDS)
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>(){
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        hashMap.put(mTag, d);
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        iRxNext.doNext(1);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 
     public interface IRxNext {
         void doNext(long number);
